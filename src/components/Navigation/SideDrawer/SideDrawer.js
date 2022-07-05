dac5a32c154f48f2433fb2217841231713155042
aminhl <amin.hlel@esprit.tn>
2022-07-05 17:50:27 +0100

    reusing_the_backdrop

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,23 @@ import React from "react";
 import classes from "./SideDrawer.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import BackDrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Auxiliary";
 
 const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) attachedClasses = [classes.SideDrawer, classes.Open];
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <BackDrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(" ")}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <NavigationItems />
       </div>
-      <NavigationItems />
-    </div>
+    </Aux>
   );
 };
 
